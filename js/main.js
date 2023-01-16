1a09d1e0bdec14afb077022923af961f1e5a5fa2
Настя <smelw@list.ru>
2023-01-16 21:25:00 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 $(".slider").slick({
     infinite: true,
     dots: false,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
     prevArrow: "<img src='./img/icons/arrow.svg' class='prev' alt='1'>",
     nextArrow: "<img src='./img/icons/arrow.svg' class='next' alt='2'>",
-    responsive: [
-      {
-        breakpoint: 1000,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+    // responsive: [
+    //   {
+    //     breakpoint: 1000,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 2,
+    //     },
+    //   },
+    //   {
+    //     breakpoint: 600,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1,
+    //     },
+    //   },
+    // ],
   });
