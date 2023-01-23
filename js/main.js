5b0c4c8db4881f57494dbc91b6da281c21ec024e
Настя <smelw@list.ru>
2023-01-23 19:06:07 +0300

    sliders

--- js/main.js
+++ js/main.js
@@ -4,7 +4,17 @@ $(".slider").slick({
   slidesToScroll: 1,
   centerMode: true,
   autoplay: true,
-  autoplaySpeed: 1000,
+  autoplaySpeed: 2000,
+  prevArrow: "<img src='./img/icons/arrow.svg' class='prev' alt='1'>",
+  nextArrow: "<img src='./img/icons/arrow.svg' class='next' alt='2'>",
+});
+
+$(".slider-reviews").slick({
+  slidesToShow: 1,
+  dots: true,
+  arrows: true,
+  slidesToScroll: 1,
+  centerMode: true,
   prevArrow: "<img src='./img/icons/arrow.svg' class='prev' alt='1'>",
   nextArrow: "<img src='./img/icons/arrow.svg' class='next' alt='2'>",
 });
