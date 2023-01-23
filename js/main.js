61279e60bb0843eb82860ef020ad17690b9c33ab
Настя <smelw@list.ru>
2023-01-23 18:29:45 +0300

    fix slider

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(".slider").slick({
   slidesToScroll: 1,
   centerMode: true,
   autoplay: true,
-  autoplaySpeed: 2000,
+  autoplaySpeed: 1000,
   prevArrow: "<img src='./img/icons/arrow.svg' class='prev' alt='1'>",
   nextArrow: "<img src='./img/icons/arrow.svg' class='next' alt='2'>",
 });
