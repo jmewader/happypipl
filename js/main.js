f50d08726aa6793bafeaf9c1f6cbd391f5b078aa
Настя <smelw@list.ru>
2023-01-24 19:12:30 +0300

    changed

--- js/main.js
+++ js/main.js
@@ -7,6 +7,21 @@ $(".slider").slick({
   autoplaySpeed: 2000,
   prevArrow: "<img src='./img/icons/arrow.svg' class='prev' alt='1'>",
   nextArrow: "<img src='./img/icons/arrow.svg' class='next' alt='2'>",
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
 });
 
 $(".slider-reviews").slick({
